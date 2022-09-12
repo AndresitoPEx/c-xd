dc2da2edb66e4cff7b71091b0c64e15b50249457
Jorge Romo <andresrt952@gmail.com>
2022-09-11 19:59:20 -0500

    ejercicios1y2

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Escribe tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
+
+
+DateTime Hora = DateTime.Now;
+Console.WriteLine(Hora.TimeOfDay.ToString());
